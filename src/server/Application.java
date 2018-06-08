14b2ba604c23175a80604ed71c86e26cb2d71e73
cloudling <2659308729@qq.com>
2018-06-08 09:45:02 +0800

    1.1

--- src/server/Application.java
+++ src/server/Application.java
@@ -2,19 +2,12 @@ package server;
 
 import java.sql.SQLException;
 
-import collect.CollectManager;
 
 
 
 public class Application {
 	public Application(){
 		startMysql();
-		try {
-			CollectManager.init(CollectManager.YEMEI);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("直播采集初始化失败:"+e.getMessage());
-		}
 	}
 	
 	public void startMysql(){

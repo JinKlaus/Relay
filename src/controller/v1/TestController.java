22a293736a159eb23dfe0ad406aa2faed737d50f
Jinxilong <602749615@qq>
2018-07-27 11:37:38 +0800

    13.0

---
+++ src/controller/v1/TestController.java
@@ -0,0 +1,23 @@
+package controller.v1;
+
+import annotation.action;
+import server.Controller;
+import server.ControllerContext;
+
+
+/**
+ * @Description TODO
+ * @Author 小云网络jxl
+ * @Date 2018-07-26  16:35
+ * @Version 1.0
+ **/
+public class TestController extends Controller {
+    public TestController(ControllerContext context) {
+        super(context);
+    }
+
+    @action
+    public void test(){
+        toHtml("admin_tpl/HD-100demo");
+    }
+}

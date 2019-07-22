95ed2cac89ab7d276157f527746dcc7d76a2b3f0
xilong.jin <xilong.jin@mirrortech.cn>
2019-07-22 15:36:16 +0800

    1.0

--- src/config/Entrance.java
+++ src/config/Entrance.java
@@ -3,10 +3,10 @@ package config;
 public class Entrance {
 	
 	//mysql 测试fu
-	public static String mysql_host = "localhost";
+	public static String mysql_host = "47.99.115.104";
 	public static String mysql_name = "relay";
 	public static String mysql_user = "root";
-	public static String mysql_pasword = "root";
+	public static String mysql_pasword = "5201314jin";
 
 	public static int java_port = 8000;
 
